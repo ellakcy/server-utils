78ae1efde16db1a9a7eec67c4cf2db75eedb6fc5
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-06-18 15:30:40 +0300

    Add Licence

--- randomport.py
+++ randomport.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#
+'''
 # Random non listening port finder
 # Copyright (C) 2016-2017 Dimitrios Desyllas
 #
@@ -16,7 +16,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+'''
+
 
 from __future__ import print_function
 import socket
